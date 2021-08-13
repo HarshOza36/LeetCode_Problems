57c4884b2b5f628b464da56a4624f4f7b84202d5
HarshOza36 <harshoza3636@gmail.com>
2021-08-13 16:01:44 +0530

    String problems

---
+++ String/P1678 - goalParserInterpretation.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        command = command.replace("()","o").replace("(al)","al")
+        return command

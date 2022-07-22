8b57d0a569008cf2c05c245b063a500ced0ce67a
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2022-07-22 16:20:11 -0700

    Update P07 - reverseInteger.py

--- Math/P07 - reverseInteger.py
+++ Math/P07 - reverseInteger.py
@@ -4,12 +4,25 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        num = x
-        x = str(abs(x))
-        out = int(x[::-1])
-        if(out > 2**31 -1 or out < -2**31):
+        # Brute force
+#         num = x
+#         x = str(abs(x))
+#         out = int(x[::-1])
+#         if(out > 2**31 -1 or out < -2**31):
+#             return 0
+#         elif(num < 0):
+#             return -1*out
+#         else:
+#             return out
+        
+        mul = -1 if x < 0 else 1
+        ans = 0
+        x = abs(x)
+        while x:
+            rem = x % 10
+            x = x // 10
+            ans = ans*10 + rem
+        ans = ans * mul
+        if ans < -2**31 or ans > 2**31 - 1:
             return 0
-        elif(num < 0):
-            return -1*out
-        else:
-            return out
+        else: return ans

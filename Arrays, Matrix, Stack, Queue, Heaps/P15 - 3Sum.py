1a5d6f80134aa8ba8253552ca4e73f5cf9d5d5d2
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2022-07-27 19:50:33 -0700

    Update P15 - 3Sum.py

--- Arrays, Matrix, Stack, Queue, Heaps/P15 - 3Sum.py
+++ Arrays, Matrix, Stack, Queue, Heaps/P15 - 3Sum.py
@@ -1,16 +1,41 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        out = set()
-        if len(nums) < 3:
-            return out
+#         out = set()
+#         if len(nums) < 3:
+#             return out
 
+#         nums.sort()
+#         for i in range(len(nums)):
+#             target = -nums[i]
+#             temp = dict()
+#             for j in range(i+1,len(nums)): 
+#                 if(target - nums[j] in temp):
+#                     out.add((nums[i],nums[j],nums[temp[target - nums[j]]]))
+#                 else:
+#                     temp[nums[j]] = j
+#         return [list(i) for i in out]
+
+    # brute force above had repetitions below is more optimized code
+    if len(nums) < 3:
+            return []
+        res = []
         nums.sort()
-        for i in range(len(nums)):
-            target = -nums[i]
-            temp = dict()
-            for j in range(i+1,len(nums)): 
-                if(target - nums[j] in temp):
-                    out.add((nums[i],nums[j],nums[temp[target - nums[j]]]))
+        
+        for i, first in enumerate(nums):
+            if i > 0 and first == nums[i-1]:
+                continue
+            l, r = i+1, len(nums) - 1
+            while(l < r):
+                threeSum = first + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    temp[nums[j]] = j
-        return [list(i) for i in out]
+                    res.append([first, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return res
+        
+    

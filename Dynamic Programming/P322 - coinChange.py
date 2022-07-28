8f7103735bd348baa6a011726b0975a827bc8262
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2022-07-28 13:45:11 -0700

    Update P322 - coinChange.py

--- Dynamic Programming/P322 - coinChange.py
+++ Dynamic Programming/P322 - coinChange.py
@@ -5,6 +5,27 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+#         # O(c * amount) time and space
+#         if amount == 0:
+#             return 0
+#         c = len(coins)
+#         dp = [[float('inf')] * (amount + 1) for _ in range(c + 1)]
+        
+#         for i in range(c+1):
+#             dp[i][0] = 1
+        
+#         for i in range(1, c+1):
+#             for j in range(1, amount + 1):
+#                 if j - coins[i-1] == 0:
+#                     dp[i][j] = 1
+#                 elif j - coins[i-1] < 0:
+#                     dp[i][j] = dp[i-1][j]
+#                 else:
+#                     dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]])
+
+#         return -1 if dp[c][amount] == float('inf') else dp[c][amount]
+        
+        # optimizing space now O(amount) space, and O(c*amount) time
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
         
